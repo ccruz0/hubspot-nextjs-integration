323ce4038d43fe4e2e0e82f0d9476bfcc05ce5b8
ccruz0 <carlos_cruz_diaz@hotmail.com>
2025-06-07 15:28:37 +0800

    Update route.ts

--- app/api/crm/contacts/route.ts
+++ app/api/crm/contacts/route.ts
@@ -1,84 +1,9 @@
-import { hubspotClient } from "@/hubspot/client"
-import { contactsCols, hasAllKeys } from "@/utils"
-import { generateCreateContactBody, TParsedContact } from "@/utils/contacts"
-import { AssociationSpecAssociationCategoryEnum, SimplePublicObjectInputForCreate } from "@hubspot/api-client/lib/codegen/crm/companies"
-import _ from "lodash"
-import type { NextApiRequest } from 'next'
+import { NextResponse } from 'next/server'
 
- 
-export async function GET(
-  req: NextApiRequest,
-) {
-  try {
-    const contacts = await hubspotClient.crm.contacts.getAll()
-    return Response.json(contacts)
-
-  } catch(e) {
-    return Response.error()
-  }
+export async function GET(req: Request) {
+  return NextResponse.json({
+    message: 'Dummy contacts response',
+    contacts: [],
+  })
 }
-
-const customStatus = {
-  pendingresponse: "184143484",
-  pendingcontract: "184204927"
-}
-
-const getDealStage = (dealStage: string) => {
-  switch(dealStage) {
-    case "pendingresponse":
-      return customStatus.pendingresponse
-    case "pendingcontract":
-      return customStatus.pendingcontract
-    default:
-      return dealStage
-  }
-}
-
-export async function PUT(
-  req: Request
-) {
-  const body = await req.json()
-  const hubspot_owner_id = body.ownerId
-  const contact = body.contact as TParsedContact
-  try {
-    if (!contact || hasAllKeys(contact, [contactsCols])) throw new Error()
-
-    const createdContact = await hubspotClient.crm.contacts.basicApi.create({
-      properties: generateCreateContactBody(contact),
-      associations: []
-    })
-    let deals
-    if (contact.deals.length > 0) {
-      const inputs = contact.deals.map(
-        deal => {
-          const input = new SimplePublicObjectInputForCreate()
-          const { properties } = deal;
-          const dealstage = getDealStage(properties.pipeline)
-          const pipeline = "default"
-          input.properties = { ...properties, dealstage, hubspot_owner_id, pipeline };
-          input.associations = [
-            {
-              to: {
-                id: createdContact.id
-              },
-              types: [
-                {
-                  associationCategory: "HUBSPOT_DEFINED" as AssociationSpecAssociationCategoryEnum,
-                  associationTypeId: 3
-                }
-              ]
-            }
-          ]
-          return input
-        }
-      )
-      deals = await hubspotClient.crm.deals.batchApi.create({
-        inputs
-      })
-    }
-    return Response.json({contact, deals})
-  } catch (e) {
-    console.log(e)
-    return Response.error()
-  }
-}
+fix: replace NextApiRequest with App Router Request
